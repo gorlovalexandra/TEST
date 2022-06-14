b52b313b9f34aa89dee29928abd2857ce534c98a
Alexandra Gorlova <gorlova.alexandra2014@yandex.ru>
2022-06-14 19:08:43 +0300

    added program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿void PrintArray(string[] text)
+{
+    int length = text.Length;
+
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"{text[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void ThreeSignStrings(string[] array)
+{
+    foreach (string element in array)
+    {
+        if (element.Length <= 3)
+            Console.Write(element + " ");
+    }
+}
+
+string[] textone = { "hello", "2", "world", ":-)" };
+string[] texttwo = { "1234", "1567", "-2", "computer science" };
+string[] textthree = { "Russia", "Denmark", "Kazan" };
+
+PrintArray(textone);
+Console.WriteLine();
+ThreeSignStrings(textone);
+Console.ReadLine();
+PrintArray(texttwo);
+Console.WriteLine();
+ThreeSignStrings(texttwo);
+Console.ReadLine();
+PrintArray(textthree);
+Console.WriteLine();
+ThreeSignStrings(textthree);
